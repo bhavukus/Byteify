e955dcc35cfd58be438e2685b6142a0ab691b05c
Rohit Kumar <rohit20021004@gmail.com>
2024-12-12 15:13:52 +0530

    js add

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
 
-let num = prompt("Enter The Number");
-
-if (num % 5 === 0 ){
-    console.log(num, "is the multipule of 5")
-}else {
-    console.log(num, "is not the multipule of 5")
-}
+let num = prompt("Enter Your Age");
